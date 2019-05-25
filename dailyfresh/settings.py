04bb809ba98901160b6fa879f3e58f7ee091c33e
lvzhaozhao <42398664+lvzhaozhao@users.noreply.github.com>
2019-05-25 23:54:26 +0800

    Update settings.py

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'dailyfresh.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "shuiguo",
-        'USER': "root",
-        'PASSWORD': "123456",
+        'NAME': "shiuguo",
+        'USER': "xxxx",
+        'PASSWORD': "xxxxx",
         'HOST': "127.0.0.1",
         'PORT': 3306,
         # 取消映射检查
